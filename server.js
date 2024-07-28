403c54dd09e5e8e138553304219815c9d616ebe2
justinscottjenecke <justinjenecke@gmail.com>
2024-07-28 20:37:56 +0200

    feature: configure json get request endpoint

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const express = require("express");
 
 const APP = express();
 
+
 APP.get("/", (req, res) => {
     res.send("<button>Hello World</button>");
 });
+
+APP.get("/json", (req, res) => {
+    res.send({
+        path: "root/json",
+        test: "hello world"
+    });
+});
+
 
 APP.listen(8888, () => console.log("Web server is running"));
